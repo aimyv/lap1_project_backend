254d03d6cfeb0345a20caefbf999ecf52d1667a4
aimyv <av533@bath.ac.uk>
2022-10-03 11:31:16 +0100

    added link to entry route in server

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const server = express()
 let cors = require("cors")
 server.use(cors());
 
+const entryRoutes = require('./controllers/entries');
+
 server.get('/', (req, res) => {
     res.send('Welcome to the Server!')
 })
+
+server.use('/entry', entryRoutes)
 
 module.exports = server;
