a113b08897fbecba9010779564e2100ffce6aad3
Ami <ami1996_tm@live.co.uk>
2022-10-03 15:33:59 +0100

    Added more test cases

--- testing/server.test.js
+++ testing/server.test.js
@@ -3,6 +3,8 @@ const server = require('../server')
 
 const port = 5000
 
+const data = require('../data')
+
 describe('API server', () => {
     let api
     beforeAll(() => {
@@ -22,17 +24,29 @@ describe('API server', () => {
         request(api).get('/').expect(200, done)
     })
 
+    test('get all data with / endpoint', (done) => {
+        request(api).get('/entries').expect(200).expect(data, done)
+    })
+
     test('get post entry by id', (done) => {
-        request(api).get('/entries/2').expect(200).expect({
-            "postId": 2,
-            "author": "Peter",
-            "title": "Second Title",
-            "content": "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing indu",
-            "comments": ["comment 1", "comment 2"],
-            "gifUrl": "https://media.giphy.com/media/3o7H5jZq2TmpnRBdOE/giphy.gif",
-            "e1": 2,
-            "e2": 0,
-            "e3": 1,    
-        }, done)
+        request(api).get('/entries/2').expect(200).expect(data[1], done)
+    })
+
+    test('get error for post entry with invalid id', (done) => {
+        request(api).get('/entries/-1').expect({message: 'This entry does not exist!'}, done)
+    })
+
+    test('post new entry', (done) => {
+        request(api).post('/entries').send({
+            "author": "John Doe",
+            "title": "Test Title",
+            "content": "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing indu"
+        }).expect(201, done)//).expect(data[data.length - 1], done). // Check id
+    })
+
+    test('update existing entry', (done) => {
+        request(api).put('/entries/2').send({
+            "comment": "This is a test comment"
+        }).expect(201, done) // Check comments length and last comment
     })
 })
