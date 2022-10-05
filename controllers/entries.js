bebc7ee6c8830aebaf7c1f0b548a9535cfef7968
karalkal <karalkal@yahoo.co.uk>
2022-10-05 13:59:40 +0100

    added pagination functionality

--- controllers/entries.js
+++ controllers/entries.js
@@ -26,6 +26,27 @@ router.get('/:id', (req, res) => {
     }
 })
 
+// pagination with 4 items displayed
+// rootURL + entries/posts/query?p=[[page number]]
+router.get('/posts/query', (req, res) => {
+    const posts = Entry.all
+
+    // console.log("total entries: ", posts.length)
+
+    const pageCount = Math.ceil(posts.length / 4);
+    // console.log("total pages // 4: ", pageCount)
+    let page = parseInt(req.query.p);
+    if (!page) { page = 1; }
+    if (page > pageCount) {
+        page = pageCount
+    }
+    res.status(200).json({
+        "page": page,
+        "pageCount": pageCount,
+        "posts": posts.slice(page * 4 - 4, page * 4)
+    });
+});
+
 router.post('/', (req, res) => {
     const data = req.body;
     const newEntry = Entry.create(data);
@@ -34,24 +55,24 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-    
+
     const entryId = parseInt(req.params.id);
-    
+
     const selectedEntry = Entry.findById(entryId)
 
-    if(req.body.comment) {
+    if (req.body.comment) {
         selectedEntry.update(entryId, "comments", req.body.comment)
-    } 
-    if(req.body.e1) {
+    }
+    if (req.body.e1) {
         selectedEntry.update(entryId, "e1", req.body.e1)
-    } 
-    if(req.body.e2) {
+    }
+    if (req.body.e2) {
         selectedEntry.update(entryId, "e2", req.body.e2)
-    } 
-    if(req.body.e3) {
+    }
+    if (req.body.e3) {
         selectedEntry.update(entryId, "e3", req.body.e3)
     }
-    
+
     // if(req.body.react)
     //     selectedEntry.update("reactions", req.body.react)
 
